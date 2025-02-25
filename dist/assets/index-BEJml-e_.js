5490e1156fa9ae517d0845982d96b7c5e692bfad
Clara-Manzano <clara.manzano.corona@gmail.com>
2025-02-25 17:03:01 +0100

    FEAT SECURITY

---
+++ dist/assets/index-BEJml-e_.js
@@ -0,0 +1,137 @@
+(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))i(s);new MutationObserver(s=>{for(const o of s)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function a(s){const o={};return s.integrity&&(o.integrity=s.integrity),s.referrerPolicy&&(o.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?o.credentials="include":s.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(s){if(s.ep)return;s.ep=!0;const o=a(s);fetch(s.href,o)}})();const u=[{name:"Eclipso",description:"Permite descubrir, crear y gestionar eventos culturales y de ocio en tu ciudad, como conciertos, exposiciones y actividades locales. Incluye autenticación segura y funciones para editar tu perfil y gestionar los eventos que crees.",imgUrl:"eclipso.webp",imgUrl2:"eclipso2.webp",url:"https://eclipso-front.vercel.app/",gitHub:"https://github.com/ClaraDevelope/eclipso",technologies:[{name:"HTML",img:"https://cdn-icons-png.flaticon.com/256/174/174854.png"},{name:"CSS",img:"https://daoliveros7.github.io/DSW-DayronOliveros/fase3/img/css.png"},{name:"JavaScript",img:"https://cdn.iconscout.com/icon/free/png-256/javascript2752148-2284965.png"},{name:"Node.js",img:"https://miro.medium.com/v2/resize:fit:900/1*TY9uBBO9leUbRtlXmQBiug.png"},{name:"MongoDB",img:"https://api.civo.com/k3s-marketplace/mongodb.png"},{name:"Express.js",img:"https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/20/express-js.png"},{name:"Vite",img:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/1200px-Vitejs-logo.svg.png"}]},{name:"HelpyHub",description:'Facilita la conexión entre usuarios interesados en proyectos sociales. Permite explorar iniciativas, comentar, dar "likes" y construir redes. No tiene backend, pero ofrece herramientas para la interacción.',imgUrl:"helpy.webp",imgUrl2:"helpy2.webp",url:"https://helpy-hub-claras-projects-939a82ac.vercel.app/",gitHub:"https://github.com/ClaraDevelope/HelpyHub",technologies:[{name:"HTML",img:"https://cdn-icons-png.flaticon.com/256/174/174854.png"},{name:"CSS",img:"https://daoliveros7.github.io/DSW-DayronOliveros/fase3/img/css.png"},{name:"React",img:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgf4FJKwNsetJENnSOiLe8GgNSc1_0Fguf4-0TBaXPmcnIPUHsUbsqfRS_0AjrxhmUDUER28Eeo-MV5aEOcaVOqLepSHNaebbzC9CBH9kl6llfnBCEmhW0E7msdffVZ5i_XR7SqpivDKz8/s1600/reactjs+tutorial.png"},{name:"Vite",img:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/1200px-Vitejs-logo.svg.png"}]},{name:"MenstruApp",description:"MenstruApp registra ciclos, gestiona síntomas y realiza cálculos de salud. Incluye funcionalidades de chat y publicación de posts para fomentar la interacción entre usuarios interesados en temas de salud menstrual.",imgUrl:"menstru.webp",imgUrl2:"menstru2.webp",url:"https://frontend-13-claras-projects-939a82ac.vercel.app/",gitHub:"https://github.com/ClaraDevelope/frontend-13",technologies:[{name:"HTML",img:"https://cdn-icons-png.flaticon.com/256/174/174854.png"},{name:"CSS",img:"https://daoliveros7.github.io/DSW-DayronOliveros/fase3/img/css.png"},{name:"React",img:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgf4FJKwNsetJENnSOiLe8GgNSc1_0Fguf4-0TBaXPmcnIPUHsUbsqfRS_0AjrxhmUDUER28Eeo-MV5aEOcaVOqLepSHNaebbzC9CBH9kl6llfnBCEmhW0E7msdffVZ5i_XR7SqpivDKz8/s1600/reactjs+tutorial.png"},{name:"Node.js",img:"https://miro.medium.com/v2/resize:fit:900/1*TY9uBBO9leUbRtlXmQBiug.png"},{name:"MongoDB",img:"https://api.civo.com/k3s-marketplace/mongodb.png"},{name:"Cloudinary",img:"https://alternative.me/media/256/cloudinary-icon-mmlef9w15ush8bou-c.png"},{name:"Express.js",img:"https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/20/express-js.png"},{name:"Socket.io",img:"https://i1.wp.com/www.ux-republic.com/wp-content/uploads/2018/03/socket.png?fit=375%2C375&ssl=1"},{name:"Vite",img:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/1200px-Vitejs-logo.svg.png"}]}],l={frontend:[{name:"HTML5",img:"https://cdn-icons-png.flaticon.com/256/174/174854.png"},{name:"CSS3",img:"https://daoliveros7.github.io/DSW-DayronOliveros/fase3/img/css.png"},{name:"JavaScript ES6",img:"https://cdn.iconscout.com/icon/free/png-256/javascript2752148-2284965.png"},{name:"React",img:"https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgf4FJKwNsetJENnSOiLe8GgNSc1_0Fguf4-0TBaXPmcnIPUHsUbsqfRS_0AjrxhmUDUER28Eeo-MV5aEOcaVOqLepSHNaebbzC9CBH9kl6llfnBCEmhW0E7msdffVZ5i_XR7SqpivDKz8/s1600/reactjs+tutorial.png"}],backend:[{name:"Node.js",img:"https://miro.medium.com/v2/resize:fit:900/1*TY9uBBO9leUbRtlXmQBiug.png"},{name:"Express.js",img:"https://adware-technologies.s3.amazonaws.com/uploads/technology/thumbnail/20/express-js.png"},{name:"Socket.io",img:"https://i1.wp.com/www.ux-republic.com/wp-content/uploads/2018/03/socket.png?fit=375%2C375&ssl=1"},{name:"MongoDB",img:"https://api.civo.com/k3s-marketplace/mongodb.png"},{name:"Cloudinary",img:"https://alternative.me/media/256/cloudinary-icon-mmlef9w15ush8bou-c.png"}],herramientas:[{name:"Git",img:"https://victorroblesweb.es/wp-content/uploads/2018/04/git.png"},{name:"Insomnia",img:"https://s3.amazonaws.com/s3.roaringapps.com/assets/icons/1561251841927-Insomnia.png"},{name:"Vite",img:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Vitejs-logo.svg/1200px-Vitejs-logo.svg.png"},{name:"Json Web Token",img:"https://media.licdn.com/dms/image/D4D12AQHSCcu92RclAw/article-cover_image-shrink_720_1280/0/1718733973021?e=2147483647&v=beta&t=TJQxW_-k7g3-ukGI-NvdEu_ui9_TeZ-GVrdp_KjjLCc"}]},m={p1:"Me llamo Clara y, aunque mi camino hacia la programación no ha sido el más convencional, cada experiencia previa ha enriquecido mi forma de abordar este ámbito. Antes de centrarme en el desarrollo web, trabajé en sectores sociales y educativos, desde la educación social hasta la sexología, además de haber gestionado una librería-café como <strong>socia trabajadora de una cooperativa.</strong>",p2:"Desde siempre me han interesado las metodologías como herramientas para organizar y entender los procesos. En el ámbito tecnológico, creo que elegir el enfoque adecuado es clave para un desarrollo eficiente y alineado con los objetivos del proyecto. Mi trayectoria en el ámbito social y educativo me ha enseñado a <strong>identificar diferentes necesidades y buscar soluciones adaptadas y sostenibles</strong>, lo que ha despertado mi interés por áreas como la accesibilidad, la calidad del software y la gestión de proyectos. Me motiva seguir explorando cómo aplicar estas perspectivas para <strong>construir productos y procesos que realmente tiendan puentes entre ideas, necesidades y objetivos.</strong>",p3:"Cuando no estoy programando, toco en una batucada de música afrobrasileña, participo en <strong>actividades culturales y sociales</strong> de mi ciudad, me gusta leer sobre diferentes temas y disfrutar de series y películas."};function h(){const t=document.getElementById("about");t.innerHTML=`
+    <section class="about">
+      <div class="container">
+        <h2>Sobre mí</h2>
+        <p>${m.p1}</p>
+        <p>${m.p2}</p>
+        <p>${m.p3}</p>
+        <img src="yo-gato.webp" alt="yo-Bolivia" class='avatar'/>
+      </div>
+    </section>
+  `}const c={_origin:"https://api.emailjs.com"},f=(t,e="https://api.emailjs.com")=>{c._userID=t,c._origin=e},p=(t,e,a)=>{if(!t)throw"The user ID is required. Visit https://dashboard.emailjs.com/admin/integration";if(!e)throw"The service ID is required. Visit https://dashboard.emailjs.com/admin";if(!a)throw"The template ID is required. Visit https://dashboard.emailjs.com/admin/templates";return!0};class d{constructor(e){this.status=e.status,this.text=e.responseText}}const g=(t,e,a={})=>new Promise((i,s)=>{const o=new XMLHttpRequest;o.addEventListener("load",({target:n})=>{const r=new d(n);r.status===200||r.text==="OK"?i(r):s(r)}),o.addEventListener("error",({target:n})=>{s(new d(n))}),o.open("POST",c._origin+t,!0),Object.keys(a).forEach(n=>{o.setRequestHeader(n,a[n])}),o.send(e)}),v=(t,e,a,i)=>{const s=i||c._userID;return p(s,t,e),g("/api/v1.0/email/send",JSON.stringify({lib_version:"3.2.0",user_id:s,service_id:t,template_id:e,template_params:a}),{"Content-type":"application/json"})},b=t=>{let e;if(typeof t=="string"?e=document.querySelector(t):e=t,!e||e.nodeName!=="FORM")throw"The 3rd parameter is expected to be the HTML form element or the style selector of form";return e},y=(t,e,a,i)=>{const s=i||c._userID,o=b(a);p(s,t,e);const n=new FormData(o);return n.append("lib_version","3.2.0"),n.append("service_id",t),n.append("template_id",e),n.append("user_id",s),g("/api/v1.0/email/send-form",n)},w={init:f,send:v,sendForm:y},E=()=>{const t=document.createElement("div");t.classList.add("alert","success");const e=document.createElement("span");e.classList.add("alert-icon"),e.innerHTML="&#10004;";const a=document.createElement("span");a.classList.add("alert-text"),a.textContent="Mensaje enviado con éxito",t.appendChild(e),t.appendChild(a),document.body.appendChild(t),setTimeout(()=>{t.remove()},4e3)},j=()=>{const t=document.createElement("div");t.classList.add("alert","error");const e=document.createElement("span");e.classList.add("alert-icon");const a=document.createElement("span");a.classList.add("alert-text"),a.textContent="Hubo un error al enviar el mensaje",t.appendChild(e),t.appendChild(a),document.body.appendChild(t),setTimeout(()=>{t.remove()},4e3)};function C(){const t=document.getElementById("contact");t.innerHTML=`
+    <section class="contact">
+      <div class="container">
+        <h2>Contacto</h2>
+        <form id="contactForm">
+          <label for="name">Nombre:</label>
+          <input type="text" id="name" name="name" required>
+
+          <label for="email">Correo electrónico:</label>
+          <input type="email" id="email" name="email" required>
+
+          <label for="message">Mensaje:</label>
+          <textarea id="message" name="message" required></textarea>
+
+          <button type="submit" class="btn-primary">Enviar mensaje</button>
+        </form>
+      </div>
+    </section>
+  `;const e=document.getElementById("contactForm");e.addEventListener("submit",function(a){a.preventDefault();const i=e.name.value,s=e.email.value,o=e.message.value,n={from_name:i,from_email:s,message:o};w.send("service_03lub14","template_p5c876e",n,"ua9jkKvxXJx73MyJa").then(function(r){console.log("Éxito!",r.status,r.text),E(),e.reset()},function(r){console.error("Error:",r),j()})})}function k(){const t=document.getElementById("footer");t.innerHTML=`
+    <footer>
+      <div class="container">
+         <p>© 2024 Clara Manzano Corona | Código abierto para aprendizaje y colaboración | <a href="https://github.com/ClaraDevelope" target="_blank">GitHub</a> | <a href="https://www.linkedin.com/in/clara-manzano-corona/" target="_blank">LinkedIn</a></p>
+      </div>
+    </footer>
+  `}function x(){const t=document.getElementById("header");t.innerHTML=`
+    <header>
+      <nav>
+        <div id="menu-icon" class="menu-icon">&#9776;</div>
+        <ul id="nav-links">
+          <li><a href="#home">Inicio</a></li>
+          <li><a href="#projects">Proyectos</a></li>
+          <li><a href="#technologies">Tecnologías</a></li>
+          <li><a href="#about">Sobre mí</a></li>
+          <li><a href="#contact">Contacto</a></li>
+        </ul>
+      </nav>
+    </header>
+  `;const e=document.getElementById("menu-icon"),a=document.getElementById("nav-links");e.addEventListener("click",()=>{a.classList.toggle("active")}),window.addEventListener("scroll",()=>{console.log("scrolling");const i=document.querySelector("header");window.scrollY>0?(console.log("Header scrolled"),i.classList.add("scrolled")):(console.log("Header not scrolled"),i.classList.remove("scrolled"))})}function S(){const t=document.getElementById("home");t.innerHTML=`
+    <section class="intro">
+      <div class="container">
+        <div class="intro-content">
+          <div class="avatar-container">
+            <img src="/yo-blur.png" alt="Clara Manzano" class="avatar">
+          </div>
+          <div class="text-content">
+            <h1>Hola, soy Clara Manzano Corona</h1>
+            <h3>Desarrolladora web full-stack</h3>
+            <p>Cada línea es un latido, y cada proyecto, una historia. ¿Construimos la siguiente?</p>
+            <div class="btn-container">
+              <a href="https://github.com/tu-github" target="_blank" class="btn-rrss"><i class="fab fa-github"></i></a>
+              <a href="https://www.linkedin.com/in/tu-linkedin" target="_blank" class="btn-rrss"><i class="fab fa-linkedin"></i></a>
+              <a href="/CV Clara Manzano. Desarrollo web.pdf" target="_blank" class="btn-rrss"><i class="fas fa-file-download"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  `}function L(){const t=document.getElementById("projects"),e=u.reverse().map(a=>`
+      <div class="project-item">
+        <div class="project-img-container">
+          <img src="${a.imgUrl}" alt="${a.name}" class="project-img first-img" />
+          ${a.imgUrl2?`<img src="${a.imgUrl2}" alt="${a.name} (segunda imagen)" class="project-img second-img" />`:""}
+        </div>
+        <h3>${a.name}</h3>
+        <p>${a.description}</p>
+        <div class="tech-stack">
+          ${a.technologies.map(i=>`
+                <span class="badge">•&nbsp; ${i.name}</span>`).join("")}
+        </div>
+        <div class="project-actions">
+  <a href="${a.url}" target="_blank" class="btn-primary">
+    <i class="fas fa-link"></i> Ver Proyecto
+  </a>
+  <a href="${a.gitHub}" target="_blank" class="btn-secondary">
+    <i class="fab fa-github"></i> Ver en GitHub
+  </a>
+</div>
+      </div>
+    `).join("");t.innerHTML=`
+    <section class="projects">
+      <div class="container">
+        <h2 class='section-title'>Proyectos</h2>
+        <div class="project-list">
+          ${e}
+        </div>
+      </div>
+    </section>
+  `}function H(){const t=document.getElementById("technologies");t.innerHTML=`
+    <section class="technologies">
+      <h2 class="section-title">Tecnologías</h2>
+      <div class="tech-container">
+        <div class="tech-category">
+          <h3>Frontend</h3>
+          <div class="tech-items">
+            ${l.frontend.map(e=>`
+              <div class="tech-item">
+                <img src="${e.img}" alt="${e.name}" class="tech-icon">
+                <p>${e.name}</p>
+              </div>
+            `).join("")}
+          </div>
+        </div>
+        <div class="tech-category">
+          <h3>Backend</h3>
+          <div class="tech-items">
+            ${l.backend.map(e=>`
+              <div class="tech-item">
+                <img src="${e.img}" alt="${e.name}" class="tech-icon">
+                <p>${e.name}</p>
+              </div>
+            `).join("")}
+          </div>
+        </div>
+        <div class="tech-category">
+          <h3>Herramientas</h3>
+          <div class="tech-items">
+            ${l.herramientas.map(e=>`
+              <div class="tech-item">
+                <img src="${e.img}" alt="${e.name}" class="tech-icon">
+                <p>${e.name}</p>
+              </div>
+            `).join("")}
+          </div>
+        </div>
+      </div>
+    </section>
+  `}x();S();L();H();h();C();k();
